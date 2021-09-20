ed0fd13361afc477552dde82eaf1a3688a28748d
caraelliott <62650005+caraelliott@users.noreply.github.com>
2021-09-20 11:47:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,34 +6,36 @@ const play = document.getElementById("play");
 const feed = document.getElementById("feed");
 const nap = document.getElementById("nap");
 const go = document.getElementById("go");
-var myPet = "Your Pet";
-function editName() {
-    myPet = prompt("Change Pet name");
-    document.querySelector("p.Pet").innerHTML = myPet;
 
-go.addEventListener("click", () =>{
-    go.style.visibility = "hidden";
-    petStats = setInterval(() => {
-    petEnergy.value -= 2;
-    petHunger.value -= 2;
-    petMood.value -= 2.5;
-    if (petEnergy.value == 0 && petHunger.value == 0){
-        petHealth.value -= 3
-    }
-    if (petHealth.value == 0){
-        clearInterval(petStats);
-        alert("Game Over!");
-        window.location.reload();
-    }
-}, 200 );
-})
+
+function editName() {
+    // this doesnt need to be in a function. this can be on its own line which would trigger the prompt to run when the page loads.
+    document.querySelector("p.Pet").innerHTML = prompt("Change Pet name")
+}
+
+// go.addEventListener("click", () =>{
+//     go.style.visibility = "hidden";
+//     petStats = setInterval(() => {
+//         petEnergy.value -= 2;
+//         petHunger.value -= 2;
+//         petMood.value -= 2.5;
+//         if (petEnergy.value == 0 && petHunger.value == 0){
+//             petHealth.value -= 3
+//         }
+//         if (petHealth.value == 0){
+//             clearInterval(petStats);
+//             alert("Game Over!");
+//             window.location.reload();
+//         }
+//     }, 200 );
+// })
 
 feed.addEventListener("click", () => {
-    petHunger.value += 6;
+    petHunger.value -= 6;
 })
 nap.addEventListener("click", () => {
-    petEnergy.value += 6;
+    petEnergy.value -= 6;
 })
 play.addEventListener("click", () => {
-    petMood.value += 6;
+    petMood.value -= 6;
 })
